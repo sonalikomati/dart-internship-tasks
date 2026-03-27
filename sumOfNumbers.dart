370dc5eb3afff148cb1dc12a04d809e469c76832
sonali komati <sonalikomati629@gmail.com>
2026-03-27 12:13:33 +0530

    Day 38 - Dart Sum of numbers program

---
+++ sumOfNumbers.dart
@@ -0,0 +1,9 @@
+//Day 38 - 27th march
+void main() {
+  List<int> num = [1, 2, 3, 4, 5];
+  int sum = 0;
+  for (int i = 0; i < num.length; i++) {
+    sum += num[i];
+  }
+  print("Sum of numbers: $sum");
+}
